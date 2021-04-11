b3c2c630627a08e71f30ee12d9be718f27f2edf5
Mateus Machado <>
2021-04-11 20:36:21 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  int a = 2;
+  std::cout << "Hello World!\n"
+  << "Hello Naninha!!!\n"
+  << "Ok entendi!!!";
+  std::cout << "\n\nMiguel!!!\n";
+  for (a=1;a<20;a++){
+    std::cout << a << "\n";
+  }
+  //
+}
